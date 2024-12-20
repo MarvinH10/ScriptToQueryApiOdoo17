cf50a3550c62a7f11dfe4e50794b21f2fd55d46b
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-19 19:07:09 -0500

    Script actualizado añadir mas tablas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const xmlrpc = require('xmlrpc');
 const { Client } = require('pg');
 const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config();
 
 const ODOO_URL = process.env.ODOO_URL;
@@ -65,130 +66,190 @@ async function fetchDatosOdoo(uid, model, domain, fields) {
             console.error('No se pudo autenticar en Odoo.');
             return;
         }
-        const campos = [
+        const camposAccountMove = [
             'id',
+            'name',
             'sequence_prefix',
             'sequence_number',
             'invoice_date',
             'create_date',
             'invoice_date_due',
             'l10n_pe_edi_operation_type',
+            'partner_id',
             'currency_id',
             'amount_untaxed',
             'amount_tax',
             'amount_total',
+            'edi_state',
+            'reversed_entry_id',
+            'ref',
+            'l10n_pe_edi_refund_reason',
         ];
+        const camposCliente = [
+            'id',
+            'name',
+            'vat',
+            'street',
+            'street2',
+            'city',
+            'state_id',
+            'country_id',
+            'zip',
+            'email',
+            'phone',
+            'mobile',
+            'website',
+            'ref',
+        ]
+        const camposAccountMoveLine = [
+            'product_id',
+        ]
         const dominio = [];
-        await fetchDatosOdoo(uid, 'account.move', dominio, campos);
+        const datosAccountMove  = await fetchDatosOdoo(uid, 'account.move', dominio, camposAccountMove);
+        const datosCliente = await fetchDatosOdoo(uid, 'res.partner', dominio, camposCliente);
+        const datosAccountMoveLine = await fetchDatosOdoo(uid, 'account.move.line', dominio, camposAccountMoveLine);
+
+        const datosCombinados = {
+            account_moves: datosAccountMove,
+            partners: datosCliente,
+            account_move_lines: datosAccountMoveLine,
+        };
+
+        const outputFile = 'datos_odoo.json';
+        fs.writeFileSync(outputFile, JSON.stringify(datosCombinados, null, 2), 'utf8');
         // console.log('Registros obtenidos:', JSON.stringify(registros, null, 2));
     } catch (error) {
         console.error('Error durante la ejecución:', error);
     }
 })();
 
-async function processData() {
-    try {
-        const uid = await autenticarEnOdoo();
-        if (!uid) {
-            console.error('No se pudo autenticar en Odoo.');
-            return;
-        }
+// async function processData() {
+//     try {
+//         const uid = await autenticarEnOdoo();
+//         if (!uid) {
+//             console.error('No se pudo autenticar en Odoo.');
+//             return;
+//         }
 
-        const campos = [
-            'id',
-            'sequence_prefix',
-            'sequence_number',
-            'invoice_date',
-            'create_date',
-            'invoice_date_due',
-            'l10n_pe_edi_operation_type',
-            'currency_id',
-            'amount_untaxed',
-            'amount_tax',
-            'amount_total',
-        ];
+//         const camposAccountMove = [
+//             'id',
+//             'name',
+//             'sequence_prefix',
+//             'sequence_number',
+//             'invoice_date',
+//             'create_date',
+//             'invoice_date_due',
+//             'l10n_pe_edi_operation_type',
+//             'partner_id',
+//             'currency_id',
+//             'amount_untaxed',
+//             'amount_tax',
+//             'amount_total',
+//             'edi_state',
+//             'reversed_entry_id',
+//             'ref',
+//             'l10n_pe_edi_refund_reason',
+//         ];
 
-        const dominio = [];
+//         const dominio = [];
 
-        const datos = await fetchDatosOdoo(uid, 'account.move', dominio, campos);
-        console.log('Datos obtenidos de Odoo:', datos);
+//         const datos = await fetchDatosOdoo(uid, 'account.move', dominio, camposAccountMove);
+//         console.log('Datos obtenidos de Odoo:', datos);
 
-        const client = new Client({
-            user: process.env.PG_USER,
-            host: process.env.PG_HOST,
-            database: process.env.PG_DATABASE,
-            password: process.env.PG_PASSWORD,
-            port: Number(process.env.PG_PORT),
-        });
+//         const client = new Client({
+//             user: process.env.PG_USER,
+//             host: process.env.PG_HOST,
+//             database: process.env.PG_DATABASE,
+//             password: process.env.PG_PASSWORD,
+//             port: Number(process.env.PG_PORT),
+//         });
 
-        await client.connect();
-        console.log('Conectado a PostgreSQL');
+//         await client.connect();
+//         console.log('Conectado a PostgreSQL');
 
-        for (const dato of datos) {
-            const query = `
-                INSERT INTO account_moves (
-                    id,
-                    sequence_prefix,
-                    sequence_number,
-                    invoice_date,
-                    create_date,
-                    invoice_date_due,
-                    operation_type,
-                    currency_id,
-                    amount_untaxed,
-                    amount_tax,
-                    amount_total
-                )
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-                ON CONFLICT (id) DO UPDATE SET
-                    sequence_prefix = EXCLUDED.sequence_prefix,
-                    sequence_number = EXCLUDED.sequence_number,
-                    invoice_date = EXCLUDED.invoice_date,
-                    create_date = EXCLUDED.create_date,
-                    invoice_date_due = EXCLUDED.invoice_date_due,
-                    operation_type = EXCLUDED.operation_type,
-                    currency_id = EXCLUDED.currency_id,
-                    amount_untaxed = EXCLUDED.amount_untaxed,
-                    amount_tax = EXCLUDED.amount_tax,
-                    amount_total = EXCLUDED.amount_total;
-            `;
+//         for (const dato of datos) {
+//             const query = `
+//             INSERT INTO account_moves (
+//                 id,
+//                 name,
+//                 sequence_prefix,
+//                 sequence_number,
+//                 invoice_date,
+//                 create_date,
+//                 invoice_date_due,
+//                 l10n_pe_edi_operation_type,
+//                 partner_id,
+//                 currency_id,
+//                 amount_untaxed,
+//                 amount_tax,
+//                 amount_total,
+//                 edi_state,
+//                 reversed_entry_id,
+//                 ref,
+//                 l10n_pe_edi_refund_reason
+//             )
+//             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+//             ON CONFLICT (id) DO UPDATE SET
+//                 name = EXCLUDED.name,
+//                 sequence_prefix = EXCLUDED.sequence_prefix,
+//                 sequence_number = EXCLUDED.sequence_number,
+//                 invoice_date = EXCLUDED.invoice_date,
+//                 create_date = EXCLUDED.create_date,
+//                 invoice_date_due = EXCLUDED.invoice_date_due,
+//                 l10n_pe_edi_operation_type = EXCLUDED.l10n_pe_edi_operation_type,
+//                 partner_id = EXCLUDED.partner_id,
+//                 currency_id = EXCLUDED.currency_id,
+//                 amount_untaxed = EXCLUDED.amount_untaxed,
+//                 amount_tax = EXCLUDED.amount_tax,
+//                 amount_total = EXCLUDED.amount_total,
+//                 edi_state = EXCLUDED.edi_state,
+//                 reversed_entry_id = EXCLUDED.reversed_entry_id,
+//                 ref = EXCLUDED.ref,
+//                 l10n_pe_edi_refund_reason = EXCLUDED.l10n_pe_edi_refund_reason;
+//         `;
 
-            const values = [
-                dato.id,
-                dato.sequence_prefix,
-                dato.sequence_number,
-                dato.invoice_date ? dato.invoice_date : null,
-                dato.create_date ? dato.create_date : null,
-                dato.invoice_date_due ? dato.invoice_date_due : null,
-                dato.l10n_pe_edi_operation_type,
-                dato.currency_id[0],
-                dato.amount_untaxed,
-                dato.amount_tax,
-                dato.amount_total,
-            ];
+//             const values = [
+//                 dato.id || null,
+//                 dato.name || null,
+//                 dato.sequence_prefix || null,
+//                 dato.sequence_number || null,
+//                 dato.invoice_date || null,
+//                 dato.create_date || null,
+//                 dato.invoice_date_due || null,
+//                 dato.l10n_pe_edi_operation_type || null,
+//                 dato.partner_id ? dato.partner_id[0] : null,
+//                 dato.currency_id ? dato.currency_id[0] : null,
+//                 dato.amount_untaxed || null,
+//                 dato.amount_tax || null,
+//                 dato.amount_total || null,
+//                 dato.edi_state || null,
+//                 dato.reversed_entry_id || null,
+//                 dato.ref || null,
+//                 dato.l10n_pe_edi_refund_reason || null,
+//             ];
 
-            await client.query(query, values);
-        }
+//             await client.query(query, values);
+//         }
 
-        console.log('Datos insertados/actualizados en la base de datos');
-        await client.end();
-    } catch (error) {
-        console.error('Error procesando datos:', error);
-    }
-}
+//         console.log('Datos insertados/actualizados en la base de datos');
+//         await client.end();
+//     } catch (error) {
+//         console.error('Error procesando datos:', error);
+//     }
+// }
 
-processData();
+// processData();
 
-async function main() {
-    try {
-        console.log("Inicio de ejecución del script...");
-        await processData();
-        console.log("Ejecución completada. Esperando 24 horas para la próxima ejecución...");
-    } catch (error) {
-        console.error("Error en el script:", error);
-    }
-}
+// async function main() {
+//     try {
+//         console.log("Inicio de ejecución del script...");
+//         await processData();
+//         console.log("Ejecución completada. Esperando 24 horas para la próxima ejecución...");
+//     } catch (error) {
+//         console.error("Error en el script:", error);
+//     }
+// }
 
-setInterval(main, 24 * 60 * 60 * 1000);
+// setInterval(main, 24 * 60 * 60 * 1000);
 
-main();
+// main();
